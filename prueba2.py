b5395972bf5c5823b55874b246b8a551764873e2
chispitas24 <nam312@alumnos.unican.es>
2022-11-17 18:06:25 +0100

    final work 17-nov

--- prueba2.py
+++ prueba2.py
@@ -22,19 +22,85 @@ def set_position(screen_name):
     screen_name.geometry("%dx%d+%d+%d" % (WIDTH, HEIGHT, x, y))
 
 
-def create_menu(screen_name):
+def create_menu():
     """Create the menu
-
-    Args:
-        screen_name (string): name of the screen
     """
-    menubar = tk.Menu(screen_name)
-    menubar.add_command(label="Menu")
-    menubar.add_command(label="Coupons")
-    menubar.add_command(label="Delivery")
-    menubar.add_command(label="Account")
-    menubar.add_command(label="My order")
-    screen_name.config(menu=menubar)
+    menu_frame = tk.Frame(bg="white", pady=20)
+
+    menu_button = tk.Button(
+        master=menu_frame,
+        text="MENU",
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    menu_button.pack(side=tk.LEFT)
+
+    coupons_button = tk.Button(
+        master=menu_frame,
+        text="COUPONS",
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+
+    )
+    coupons_button.pack(side=tk.LEFT)
+
+    # Imagen con el logo
+    photo = tk.PhotoImage(file=r"./media/logo_general.png")
+    photoimage_1 = photo.subsample(1, 2)
+
+    mainmenu_button = tk.Button(
+        master=menu_frame,
+        image=photoimage_1,
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    mainmenu_button.pack(side=tk.LEFT)
+
+    delivery_button = tk.Button(
+        master=menu_frame,
+        text="DELIVERY",
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    delivery_button.pack(side=tk.LEFT)
+
+    account_button = tk.Button(
+        master=menu_frame,
+        text="MY ACCOUNT",
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    account_button.pack(side=tk.LEFT)
+
+    photo = tk.PhotoImage(file=r"./media/carrito.png")
+    photoimage_2 = photo.subsample(8, 8)
+    order_button = tk.Button(
+        master=menu_frame,
+        image=photoimage_2,
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    order_button.pack(side=tk.LEFT)
+
+    menu_frame.pack()
 
 
 def main():
@@ -45,9 +111,16 @@ def main():
 
     # Set position on screen and size
     set_position(home)
-    #create_menu(home)
+    create_menu()
+    label = tk.Label(
+        text="↓ VIEW MORE ↓", foreground="orange", background="black", font="Helvetica 18 bold"
+    )
+    label.pack(side="bottom")
 
-    menu_frame = tk.Frame(bg="white")
+    home.mainloop()
+
+"""
+    menu_frame = tk.Frame(bg="white", pady=20)
 
     menu_button = tk.Button(
         master=menu_frame,
@@ -55,7 +128,8 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
     )
     menu_button.pack(side=tk.LEFT)
 
@@ -65,7 +139,9 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
+
     )
     coupons_button.pack(side=tk.LEFT)
 
@@ -79,7 +155,8 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
     )
     mainmenu_button.pack(side=tk.LEFT)
 
@@ -89,7 +166,8 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
     )
     delivery_button.pack(side=tk.LEFT)
 
@@ -99,7 +177,8 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
     )
     account_button.pack(side=tk.LEFT)
 
@@ -111,21 +190,11 @@ def main():
         highlightthickness=0,
         bd=0,
         font="Helvetica 30 bold",
-        bg="white"
+        bg="white",
+        padx=50
     )
     order_button.pack(side=tk.LEFT)
 
     menu_frame.pack()
-
-
-
-    label = tk.Label(
-        text="↓ VIEW MORE ↓", foreground="orange", background="black", font="Helvetica 18 bold"
-    )
-    label.pack(side="bottom")
-
-    home.mainloop()
-
-
-
+"""
 main()
