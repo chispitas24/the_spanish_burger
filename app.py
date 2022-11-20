215955c73ba52ecb972e0e0469b3a8f9e46732cd
chispitas24 <nam312@alumnos.unican.es>
2022-11-20 17:18:52 +0100

    Some updates and trying copilot

--- app.py
+++ app.py
@@ -1,71 +1,103 @@
 import tkinter as tk
-from tkinter import *
 
-gui = tk.Tk()
+from PIL import Image, ImageTk
 
-# Global variables
-WIDHT = 700
-HEIGHT = 400
-global screen_width
-global screen_height
-global x
-global y
+# Constants
+WIDTH = 2000
+HEIGHT = 1800
+
+home = None
 
 
-def menu_window():
-    new_window = Toplevel(gui)
-    new_window.geometry("750x250")
-    new_window.title("Menu | The Spanish Burger")
-    new_window.geometry("%dx%d+%d+%d" % (WIDHT, HEIGHT, x, y))
+def set_position(screen_name):
+    """Center window in the screen
+
+    Args:
+        screen_name (Tk): name of the screen
+    """
+    screen_width = screen_name.winfo_screenwidth()
+    screen_height = screen_name.winfo_screenheight()
+    x = (screen_width / 2) - (WIDTH / 2)
+    y = (screen_height / 2) - (HEIGHT / 2)
+    screen_name.geometry("%dx%d+%d+%d" % (WIDTH, HEIGHT, x, y))
 
 
-def menu_coupons():
-    new_window = Toplevel(gui)
-    new_window.geometry("750x250")
-    new_window.title("Coupons | The Spanish Burger")
-    new_window.geometry("%dx%d+%d+%d" % (WIDHT, HEIGHT, x, y))
+def create_button(text, frame_name):
+    """Create a button
+
+    Args:
+        text (string): text of the button
+        frame_name (Frame): name of the frame
+    """
+    button = tk.Button(
+        master=frame_name,
+        text=text,
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    button.pack(side=tk.LEFT)
 
 
-def menu_delivery():
-    new_window = Toplevel(gui)
-    new_window.geometry("750x250")
-    new_window.title("Delivery | The Spanish Burger")
-    new_window.geometry("%dx%d+%d+%d" % (WIDHT, HEIGHT, x, y))
+def create_button_image(image_name, frame_name):
+    """Create a button
+
+    Args:
+        image_name (Photo image): name of the image
+        frame_name (Frame): name of the frame
+    """
+    button = tk.Button(
+        master=frame_name,
+        image=image_name,
+        highlightthickness=0,
+        bd=0,
+        font="Helvetica 30 bold",
+        bg="white",
+        padx=50
+    )
+    button.pack(side=tk.LEFT)
 
 
-def menu_account():
-    new_window = Toplevel(gui)
-    new_window.geometry("750x250")
-    new_window.title("Account | The Spanish Burger")
-    new_window.geometry("%dx%d+%d+%d" % (WIDHT, HEIGHT, x, y))
+def create_menu():
+    """Create the menu
+    """
+    menu_frame = tk.Frame(bg="white", pady=20)
+
+    create_button("MENU", menu_frame)
+    create_button("COUPONS", menu_frame)
+
+    # Imagen con el logo
+    photo = tk.PhotoImage(file=r"./media/logo_general.png")
+    pic = photo.subsample(1, 2)
+    create_button_image(pic, menu_frame)
+
+    create_button("DELIVERY", menu_frame)
+    create_button("MY ACCOUNT", menu_frame)
+
+    photo = tk.PhotoImage(file=r"./media/carrito.png")
+    pic = photo.subsample(8, 8)
+    create_button_image(pic, menu_frame)
+
+    menu_frame.pack()
 
 
 def main():
+    """Main program"""
+    home = tk.Tk()  # Main page
+    home.title("Home | The Spanish Burger")
+    home.configure(background="black")
 
-    gui.title("Home | The Spanish Burger")
+    # Set position on screen and size
+    set_position(home)
+    create_menu()
+    label = tk.Label(
+        text="↓ VIEW MORE ↓", foreground="orange", background="black", font="Helvetica 18 bold"
+    )
+    label.pack(side="bottom")
 
-    screen_width = gui.winfo_screenwidth()
-    screen_height = gui.winfo_screenheight()
-    x = (screen_width / 2) - (WIDHT / 2)
-    y = (screen_width / 2) - (HEIGHT / 2)
-    gui.geometry("%dx%d+%d+%d" % (WIDHT, HEIGHT, x, y))
-    # Creating Menubar
-    menubar = Menu(gui)
-
-    # Creating labels for menu
-    menubar.add_command(label="Menu", command=menu_window)
-    menubar.add_command(label="Coupons", command=menu_coupons)
-    menubar.add_command(label="Delivery", command=menu_delivery)
-    menubar.add_command(label="Account", command=menu_account)
-    menubar.add_command(label="My order")
-
-    # display Menu
-    gui.config(menu=menubar)
-
-    # Create label
-    label = tk.Label(gui, text="See more products", foreground="orange")
-    label.pack()
-    gui.mainloop()
+    home.mainloop()
 
 
 main()
