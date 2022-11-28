5cde0e95687b7c2050676cc67301350784c65c27
chispitas24 <nam312@alumnos.unican.es>
2022-11-28 17:48:23 +0100

    Menú principal, carta, cupones y carrito funcionales. Falta cuenta y delivery

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ HEIGHT = 800
 home = None
 
 # List of products: name, price, quantity
-menu_items = (["burger", 5.2, 0], ["pizza", 8.9, 0], ["nachos", 6.4, 0])
+menu_items = (["La Nicky Ñam", 9.99, 0], ["pizza", 8.9, 0], ["nachos", 6.4, 0])
 
 
 def set_position(screen_name):
@@ -79,7 +79,10 @@ def main():
     set_position(win)
 
     # Bottom frame, for page content
-    bottom_frame = tk.Frame(padx=50, bg="black")
+    bottom_frame = tk.Frame(
+        padx=50,
+        bg="black"
+    )
 
     # Create top navigation bar
     top_bar_frame = tk.Frame(bg="white", pady=20)
@@ -104,17 +107,18 @@ def main():
 def change_screen(new_screen, frame):
     for widget in frame.winfo_children():
         widget.destroy()
+
     if new_screen == 'MENU':
         for menu_item in menu_items:
             create_product(menu_item, frame)
     elif new_screen == 'COUPONS':
-        create_home(frame)
+        create_coupons(frame)  # TODO: CREATE COUPONS
     elif new_screen == 'HOME':
         create_home(frame)
     elif new_screen == 'DELIVERY':
-        create_home(frame)
+        create_home(frame) # TODO: CREATE DELIVERY SCREEN
     elif new_screen == 'ACCOUNT':
-        create_home(frame)
+        create_home(frame) # TODO: CREATE ACCOUNT SCREEN
     elif new_screen == 'CART':
         create_order(frame)
     else:
@@ -134,28 +138,29 @@ def create_home(frame):
     resize_image = image.resize((450, 450))
     img_pizza = ImageTk.PhotoImage(resize_image)
 
-    albacete = tk.Label(image=img_albacete, master=frame)
+    frame_photos = tk.Frame(
+        master=frame,
+        padx=50,
+        pady=50,
+        background="black"
+    )
+    frame_photos.pack(side=tk.TOP, padx=50, pady=20)
+
+    albacete = tk.Label(image=img_albacete, master=frame_photos)
     albacete.image = img_albacete
-    albacete.pack(padx=50, side=tk.LEFT)
-    nachos = tk.Label(image=img_nachos, master=frame)
+    albacete.pack(side=tk.LEFT)
+    nachos = tk.Label(image=img_nachos, master=frame_photos)
     nachos.image = img_nachos
     nachos.pack(padx=50, side=tk.LEFT)
-    pizza = tk.Label(image=img_pizza, master=frame)
+    pizza = tk.Label(image=img_pizza, master=frame_photos)
     pizza.image = img_pizza
-    pizza.pack(padx=50, side=tk.LEFT)
-
-    # Create "view more" frame
-    label = tk.Label(
-        text="↓ VIEW MORE ↓",
-        foreground="white",
-        background="black",
-        font="Helvetica 18 bold",
-        pady=100,
-    )
-    label.pack(side=tk.BOTTOM)
+    pizza.pack(side=tk.LEFT)
 
     # Create "ADD TO CART" section
     frame_add_to_cart = tk.Frame(
+        master=frame,
+        padx=50,
+        pady=50,
         bg="black",
     )
 
@@ -183,11 +188,24 @@ def create_home(frame):
         background="black",
         foreground="orange",
     )
-    frame_add_to_cart.pack(side=tk.BOTTOM)
     add_to_cart_button_1.pack(side=tk.LEFT)
     add_to_cart_button_2.pack(side=tk.LEFT, padx=80)
     add_to_cart_button_3.pack(side=tk.LEFT)
-    frame.pack(padx=60, pady=30)
+    frame_add_to_cart.pack(side=tk.TOP)
+
+    # Create "view more" frame
+    label = tk.Label(
+        master=frame,
+        text="↓ VIEW MORE ↓",
+        foreground="white",
+        background="black",
+        font="Helvetica 18 bold",
+        pady=100,
+    )
+    label.pack(side=tk.BOTTOM)
+
+    frame.pack(padx=60)
+
 
 def create_product(product, frame_name):
     button = tk.Button(
@@ -205,15 +223,119 @@ def create_product(product, frame_name):
 def buy_product(product):
     product[2] += 1
 
+def create_coupons(frame):
+    label_title = tk.Label(
+        master=frame,
+        text="COUPONS",
+        foreground="orange",
+        background="black",
+        font="Helvetica 25 bold",
+        pady=20,
+    )
+    label_title.pack(side=tk.TOP)
+
+    image = Image.open(r"./media/kevin_bacon.jpg")
+    resize_image = image.resize((450, 450))
+    img_kevin = ImageTk.PhotoImage(resize_image)
+
+    image = Image.open(r"./media/kiki_burger.jpg")
+    resize_image = image.resize((450, 450))
+    img_albacete = ImageTk.PhotoImage(resize_image)
+
+    image = Image.open(r"./media/jaen_pizza.jpg")
+    resize_image = image.resize((450, 450))
+    img_jaen = ImageTk.PhotoImage(resize_image)
+
+    frame_photos = tk.Frame(
+        master=frame,
+        padx=50,
+        pady=20,
+        background="black"
+    )
+    frame_photos.pack(side=tk.TOP, padx=50, pady=20)
+
+    frame_prizes = tk.Frame(
+        master=frame,
+        background="black"
+    )
+    label_prize_kevin_bacon = tk.Label(
+        master=frame_prizes,
+        text="11'99€",
+        font="Helvetica 18 bold",
+        foreground="orange",
+        background="black"
+    )
+    label_prize_albacete = tk.Label(
+        master=frame_prizes,
+        text="16'99€",
+        font="Helvetica 18 bold",
+        foreground="orange",
+        background="black"
+    )
+    label_prize_jaen = tk.Label(
+        master=frame_prizes,
+        text="18'99€",
+        font="Helvetica 18 bold",
+        foreground="orange",
+        background="black"
+    )
+    label_prize_kevin_bacon.pack(side=tk.LEFT)
+    label_prize_albacete.pack(side=tk.LEFT, padx=425)
+    label_prize_jaen.pack(side=tk.LEFT)
+    frame_prizes.pack(side=tk.BOTTOM)
+
+    kevin = tk.Label(image=img_kevin, master=frame_photos)
+    kevin.image = img_kevin
+    kevin.pack(side=tk.LEFT)
+    albacete = tk.Label(image=img_albacete, master=frame_photos)
+    albacete.image = img_albacete
+    albacete.pack(padx=50, side=tk.LEFT)
+    jaen = tk.Label(image=img_jaen, master=frame_photos)
+    jaen.image = img_jaen
+    jaen.pack(side=tk.LEFT)
+
+    frame_text_1 = tk.Frame(
+        master=frame,
+        background="black"
+    )
+    label_kevin_bacon = tk.Label(
+        master=frame_text_1,
+        text="KEVIN BACON\n+\nCRUZCAMPO\n",
+        font="Helvetica 18 bold",
+        foreground="white",
+        background="black"
+    )
+    label_albacete = tk.Label(
+        master=frame_text_1,
+        text="ALBACETE\n+\nSANGRÍA\n",
+        font="Helvetica 18 bold",
+        foreground="white",
+        background="black"
+    )
+    label_jaen = tk.Label(
+        master=frame_text_1,
+        text="JAÉN PIZZA\n+\nNACHITOS\n",
+        font="Helvetica 18 bold",
+        foreground="white",
+        background="black"
+    )
+    label_kevin_bacon.pack(side=tk.LEFT)
+    label_albacete.pack(side=tk.LEFT, padx=350)
+    label_jaen.pack(side=tk.LEFT)
+    frame_text_1.pack(side=tk.BOTTOM)
+
+
+
 
 def create_order(frame):
-    tk.Label(master=frame, text='Your Products')
+    label = tk.Label(master=frame, text='Your Products', foreground="white").pack()
     total_cost = 0.0
     for product in menu_items:
-        tk.Label(master=frame,
-                 text=product[2] + "\tx\t" + product[0] + "\t\t" + product[1]+"€")
+        label_for = tk.Label(master=frame,
+                 text=str(product[2]) + "\tx\t" + product[0] + "\t\t" + str(product[1]) + "€").pack()
         total_cost += product[1]
-    tk.Label(master=frame, text='Total cost: '+str(total_cost)+"€")
+    label2 = tk.Label(master=frame, text='Total cost: ' + str(total_cost) + "€").pack()
+
 
 
 main()
